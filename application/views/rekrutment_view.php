2cc30264c63b6383776cdd4f8576a2cc9686776f
FeliaNovitasari <felianovitasari@gmail.com>
2019-04-08 13:44:43 +0700

    first commit

---
+++ application/views/rekrutment_view.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="pegawai/dist/css/bootstrap.css">
+    <title>Pendaftaran Global Intermedia</title><!-- <link rel="stylesheet" href="pegawai/dist/css/justified-nav.css"> -->
+    <link rel="stylesheet" href="css/global.css">
+</head>
+<body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Aplikasi Recruitment</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          
+          <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown"> 
+                <i class="glyphicon glyphicon-th-large"></i> Login <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="pkl/admin_pkl/index.php"><i class="glyphicon glyphicon-user"></i> Admin PKL</a></li>
+                <li><a href="pegawai/admin_pegawai/index.php"><i class="glyphicon glyphicon-user"></i> Admin Pegawai</a></li>
+                
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+        </div>
+        
+        <div class="container">
+            <div class="jumbotron">
+                        <img src="img/bg_top.jpg" alt="header">
+                    </div>
+            <div class="row">
+                <div class="col-md-9 col-sm-9">
+                    <div class="wrap-large">
+                        <h2>Selamat Datang</h2>
+                        <p>Di lingkungan kerja Global Intermedia, 
+                            kemampuan dalam hal Teknologi Informasi dengan kemampuan dalam melakukan interkasi sosial baik secara internal di dalam perusahaan, 
+                            maupun secara menyeluruh kepada masyarakat, 
+                            digabung menjadi satu untuk membentuk Global Intermedia yang solid yang mampu dekat dengan seluruh lapisan masyarakat.</p>
+                        <p>
+                            Global Intermedia menyambut baik bagi individu-individu yang memiliki minat dan bakat di dunia teknologi informasi untuk ikut berpartisipasi dan berkembang bersama.
+                        </p>    
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <div class="panel panel-default">
+                        <!-- Default panel contents -->
+                        <div class="panel-heading orange">Silahkan Pilih Halaman User</div>
+                        <div class="panel-body">
+                        
+                        <a class="btn btn-primary btn-sm full" href="pegawai/index.php">
+                            <i class="glyphicon glyphicon-user"></i> Daftar Pegawai</a> 
+                        <a class="btn btn-info btn-sm full" href="pkl/index.php">
+                            <i class="glyphicon glyphicon-user"></i> Daftar Prakerin/PKL</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                    
+            <footer>
+            <p>© Global Intermedia</p>
+            </footer>
+        </div>    
+        <script type="text/javascript" src="pegawai/dist/js/jquery.min.js"></script>
+        <script type="text/javascript" src="pegawai/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
+        
+        
