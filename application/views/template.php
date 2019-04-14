ebd0e926ca4f4259dbefad94fbccab88d81fd4ad
FeliaNovitasari <felianovitasari@gmail.com>
2019-04-14 20:52:19 +0700

    edit_tamp

--- application/views/template.php
+++ application/views/template.php
@@ -11,7 +11,7 @@
         <title><?= $title ?></title>
         <style>
             body {
-                background-color: #ebebeb;
+                background-color: #B43522;
             }
             a, a:hover, .mb-0 {
                 color: #e4422b;
@@ -107,20 +107,23 @@
             }
             .nav-link:hover{
                 color:rgb(146, 0, 0);
+            
             }
         </style>
     </head>
     <body>
+    <div class="container col-sm-8" style="background-color : #B43522; box-shadow : 15px;">
         <div class="container">
             <header class="web-header py-3">
                 <div class="row flex-nowrap justify-content-between align-items-center">
-                    <div class="col-12 text-center">
-                        <a class="web-header-logo text-dark" href="https://getbootstrap.com/docs/4.3/examples/blog/#">GlobalIntermedia</a>
+                    <div class="col-12 text-right">
+                    <img src="<?= base_url('assets/img/logo.jpg') ?>" alt="" style="hight: 65px; width: 250px;" >
+                        <!-- <a class="web-header-logo text-dark" href="https://getbootstrap.com/docs/4.3/examples/blog/#">GlobalIntermedia</a> -->
                     </div>
                 </div>
             </header>
             
-            <nav class="nav justify-content-center">
+            <nav class="nav justify-content-left">
                 <ul class="nav nav-tabs">
                     <li class="nav-item">
                         <a class="nav-link" href="<?= site_url('C_main_view') ?>">Beranda</a>
@@ -190,6 +193,7 @@
                 <p>PT. Global Intermedia Nusantara</p>
                 <p>Jl. Taman Siswa No. 125 Yogyakarta 55151 </p>
             </footer>
+        </div>
         </div>
         <script src="carousel/jquery-3.3.1.slim.min.js.download" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script>window.jQuery || document.write('<script src="/docs/4.3/assets/js/vendor/jquery-slim.min.js"><\/script>')</script><script src="<?= base_url('assets/custom/carousel/bootstrap.bundle.min.js.download') ?>" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
