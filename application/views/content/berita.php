7a5b0c4444c926afc3c01d15f210d447bc2ba8e3
aktamaa <aktama.akbar@outlook.co.id>
2019-04-18 07:02:51 +0700

    beritx fixed

--- application/views/content/berita.php
+++ application/views/content/berita.php
@@ -1,1 +1,162 @@
-berita
+<br>
+<h3 style="background-color:#B23322; color: white; font-family: Trebuchet MS; padding-left: 10px; margin-block-start: 0.67em; margin-block-end: 0.67em;">Berita</h1><br>
+<ul class="pagination">
+	Halaman :
+	<li><a href="#">1</a></li>
+	<li><a href="#">2</a></li>
+	<li><a href="#">3</a></li>
+	<li><a href="#">4</a></li>
+	<li><a href="#">5</a></li>
+</ul>
+<div>
+	<p><a href="#">Lowongan Pekerjaan Web Programmer (WB) dan Database Administrator(DBA)</a></p>
+
+	<p>8 Februari 2019</p>
+
+	 <strong>WEB PROGRAMMER (WB)</strong>
+	<p>Kualifikasi:</p>
+	<p>1. Pendidikan Min D3 Teknik Informatika/Elektro/Ilmu Komputer</p>
+	<p>2. Menguasai dan familiar dengan PHP, MySQL, CSS, HTML dan JavaScript</p>
+	<p>3. Menguasai pemgrograman Python sebagai nilai tambah</p>
+	<p>4. Mampu bekerja dalam tim dan jadwal yang ketat</p>
+	<p>5. Memiliki kemampuan komunikasi yang baik</p>
+	<p>6. Memiliki kemauan belajar</p>
+	<p>7. Pengalaman dalam mengembangkan aplikasi berbasis web menjadi nilai tambah</p>
+	<p>&nbsp;</p>
+	<p><strong>DATABASE ADMINISTRATOR (DBA)</strong></p>
+	<p>Kualifikasi:</p>
+	<p>Pendidikan Min. S1 Teknik Informatika/Elektro/Ilmu Komputer</p>
+	<p>Menguasai dan familiar menggunakan functions, triggeres, views, queries dan replication pada PostgreSQL</p>
+	<p>Memiliki ketrampilan pemecahan masalah yang baik</p>
+	<p>Mampu bekerja dalam tim dan jadwal yang ketat</p>
+	<p>Memiliki kemampuan komunikasi yang baik</p>
+	<p>&nbsp;</p>
+	<p>Kirim CV + Lamaran Lengkap (Ijazah, Transkrip NIlai, KTP, Sertifikat Pendukung, Portofolio, Foto berwarna) ke email <strong>hcm@gi.co.id</strong>, dengan mencantumkan kode jabatan pada subject email.</p>
+	<p>Paling lambat 8 Maret 2019</p>	
+</div>
+<br>
+<div>
+	<p><a href='#'>Lowongan Pekerjaan </a></p>
+	<p><b>12 Oktober 2018</b></p>
+		<p><p><strong>MEMBUTUHKAN</strong></p>
+        <p>ANDROID PROGRAMMER (AP):</p>
+        <ol>
+            <li>Min D3 Teknik Informatika/Elektro/Ilmu Komputer</li>
+            <li>Menguasai pemrograman menggunakan Android Studio (Java, Kotlin)</li>
+            <li>Menguasai konseppemrograman berorientasi objek</li>
+            <li>Berpengalaman menggunakan API, JSON, XML</li>
+            <li>Menguasai SQL merupakan nilai tambah</li>
+            <li>Pengalaman dalam mengembangkan aplikasi erbasis android menjadi nilai tambah</li>
+            <li>Mampu bekerja dalam tim dan jadwal yang ketat</li>
+            <li>Memiliki kemampuan komunikasi yang baik</li>
+        </ol>
+        <p>TECHNICAL SUPPORT (TS):</p>
+        <ol>
+            <li>Min D3 Teknik Informatika/Elektro/Ilmu Komputer</li>
+            <li>Kemampuan dalam mengoperasikan perangkat Mikrotik menjadi nilai tambah</li>
+            <li>Memiliki kemampuan komunikasi yang baik</li>
+            <li>Memiliki kemauan belajar</li>
+            <li>Menguasai/familiar dengan komputer</li>
+            <li>Bersedia ditempatkan di seluruh wilayah Indonesia</li>
+        </ol>
+        <p>ACCOUNTING (AKT):</p>
+        <ol>
+            <li>Min D3 Akuntansi</li>
+            <li>Diutamakan akuntansi sektor Pemerintahan</li>
+            <li>Memiliki kemampan komunikasi yang baik</li>
+            <li>Memiliki kemauan belajar</li>
+            <li>Menguasai / familiar dengan komputer</li>
+            <li>Bersedia ditempatkan di seluruh wilayah Indonesia</li>
+        </ol>
+        <p>Registrasi lamaran melalui online website atau email:</p>
+        <p>www.gi.co.id atau hcm@gi.co.id</p>
+        <p>LAMARAN DITERIMA PALING LAMBAT TANGGAL 9 NOVEMBER 2018</p></p>
+</div>
+<br>
+<div>
+	<p><a href='//www.gi.co.id/detail/news/83/lowongan-pekerjaan-web-programmer-dan-desktop-programmer.htm'>Lowongan Pekerjaan Web Programmer dan Desktop Programmer</a></p>
+						<p><b>05 Oktober 2018</b></p>
+						<p><p>MEMBUTUHKAN</p>
+
+<div>Web Programmer (WB)</div>
+<div>1.<span style="white-space:pre">	</span>Pendidikan SMK/D3/S1 Teknik Informatika/Sistem Informatika/Teknik Elektro/Ilmu Komputer</div>
+<div>2.<span style="white-space:pre">	</span>Menguasai dan familiar dengan PHP, MySQL, CSS, HTML, Javascript,</div>
+<div>3.<span style="white-space:pre">	</span>Mampu bekerja dalam tim dan jadwal yang ketat,</div>
+<div>4.<span style="white-space:pre">	</span>Memiliki kemampuan komunikasi yang baik,</div>
+<div>5.<span style="white-space:pre">	</span>Memiliki kemauan untuk belajar, pengalaman dalam mengembangkan mobile app dan web based application adalah nilai tambah.</div>
+<div>&nbsp;</div>
+<div>&nbsp;</div>
+<div>Desktop Programmer (DT)</div>
+<div>&nbsp;</div>
+<div>1.<span style="white-space:pre">	</span>Pendidikan SMK/D3/S1 Teknik Informatika/Sistem Informatika/Teknik Elektro/Ilmu Komputer</div>
+<div>2.<span style="white-space:pre">	</span>Menguasai dan familiar dengan Delphi/MySQL,</div>
+<div>3.<span style="white-space:pre">	</span>Pengalaman Pengembangan aplikasi berbasis desktop,</div>
+<div>4.<span style="white-space:pre">	</span>Mampu bekerja dalam tim dan jadwal yang ketat,</div>
+<div>5.<span style="white-space:pre">	</span>Memiliki kemampuan komunikasi yang baik.</div>
+<div>&nbsp;</div>
+<div>&nbsp;</div>
+<div>&nbsp;</div>
+<div>&nbsp;</div>
+<div>&nbsp;</div>
+<div>Registrasi lamaran melalui online website atau email&nbsp; :</div>
+<div>www.gi.co.id atau&nbsp; hcm@gi.co.id</div>
+<div>&nbsp;</div>
+<div>diterima paling lambat</div>
+<div>16 Oktober 2018</div>
+<div>&nbsp;</div></p>
+<!--</div>
+</div></p></div><div>-->
+						<p><a href='#'>Pengumuman Peserta Test Tertulis dan Wawancara PT Global Intermedia Nusantara</a></p>
+						<p><b>09 Januari 2018</b></p>
+						<p><p><span style="font-size: 14.6667px;">DAFTAR PESERTA TES WAWANCARA</span></p></p></div><div>
+						<p><a href='#'>Lowongan Kerja Global Intermedia Januari 2018</a></p>
+						<p><b>18 Desember 2017</b></p>
+						<p><p>Lowongan Pekerjaan Global Intermedia</p>
+<p>Syarat Umum:</p>
+<ul>
+    <li>Mampu Bekerja Dalam Tim dan Jadwal Yang Padat</li>
+    <li>Memiliki Kemampuan Komunikasi Yang Baik</li>
+    <li>Bersedia Ditempatkan Diseluruh Wilayah Indonesia</li>
+</ul></p></div><div>
+						<p><a href='#'>Lowongan Kerja 18 Agustus 2017 - 9 September 2017</a></p>
+						<p><b>18 Agustus 2017</b></p>
+						<p><p>&nbsp;Lowongan Kerja Global Intermedia</p>
+<p>Syarat Umum:</p>
+<ul>
+    <li>Mampu bekerja dalam tim dan jadwal yang padat</li>
+    <li>Memiliki kemampuan komunikasi yang baik</li>
+    <li>memiliki kemampuan untuk belajar</li>
+    <li>Bersedia ditempatkan diseluruh wilayah Indonesia, Khususnya Papua dan Papua Barat</li>
+</ul></p></div><div>
+						<p><a href='#'>Lowongan Kerja 21 Januari 2017 - 15 Februari 2017</a></p>
+						<p><b>23 Januari 2017</b></p>
+						<p><p>Lowongan Pekerjaan Global Intermedia</p>
+<p><strong>Syarat Umum:</strong></p>
+<ul>
+    <li>Mampu bekerja dalam tim dan jadwal yang padat</li>
+    <li>Memiliki kemampuan komunikasi yang baik</li>
+    <li>Memiliki kemauan untuk belajar</li>
+    <li>Bersedia ditempatkan diseluruh wilayah indonesia, khususnya Indonesia Timur</li>
+</ul></p></div><div>
+						<p><a href='#'>Lowongan Kerja Global Intermedia Juli 2016</a></p>
+						<p><b>14 Juli 2016</b></p>
+						<p><p>Lowongan Pekerjaan Global Intermedia<br />
+<br />
+Syarat Umum :<br />
+<br />
+- Mampu bekerja dalam tim<br />
+<br />
+- Memiliki kemauan untuk belajar<br />
+<br />
+- Bersedia ditempatkan diseluruh wilayah indonesia<br />
+<br />
+- Mampu bekerja dalam tim dan jadwal yang ketat<br />
+<br />
+- Memiliki kemampuan komunikasi yang baik</p></p></div><div>
+						<p><a href='#'>Launching Aplikasi Pendaftaran Akta Kelahiran Online Kota Yogyakarta </a></p>
+						<p><b>07 Juni 2016</b></p>
+<p>Global Intermedia dan Dinas Kependudukan dan Pencatatan Sipil Kota  Yogykarta bekerja sama dalam pembuatan Sistem Aplikasi Layanan Online Adminduk  dan pada tanggal 21 Maret 2016 Pemerintah Kota Yogyakarta yang diwakili  oleh Sekda Kota Yogyakarta meluncurkan Aplikasi Layanan Online Adminduk  agar masyarakat memperoleh kemudahan di bidang waktu, biaya, dan  aktifitas dalam mengurus akta kelahiran. Aplikasi ini bisa diakses dari  manapun,... (selengkapnya ada pada blog.gi.co.id)</p>
+</p></div><div>
+						<p><a href='#'>Pengumuman Calon Karyawan PT Global Intermedia Nusantara Maret 2016</a></p>
+						<p><b>11 Maret 2016</b></p>
+						<p><p>&nbsp;<span id="docs-internal-guid-2be40576-64f4-3329-f04d-2311c197fd2e" style="font-size: 14.6667px; font-family: Calibri; vertical-align: baseline;">DAFTAR PESERTA TEST TERTULIS &amp; WAWANCARA MARET 2016</span></p></p></div>
