2c6a22158c003faf5cbd57743f399ce37a56a425
aldrinmarbun <aldrinm76@gmail.com>
2019-04-23 16:49:42 +0700

    buat edit profile

--- application/views/V_dpegawai.php
+++ application/views/V_dpegawai.php
@@ -23,7 +23,7 @@
           <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" id="clr"><ion-icon name="logo-windows"></ion-icon>  Login Calon Pegawai</a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" class="load-modal" href="#" data-toggle="modal" data-target="#exampleModal"> <ion-icon name="person"></ion-icon> Sign in</a></li>
-            <li><a class="dropdown-item" class="load-modal" href="#"><ion-icon name="person"></ion-icon> Daftar Akun</a></li>
+            <li><a class="dropdown-item" class="load-modal" href="#" data-toggle="modal" data-target="#registerModal"><ion-icon name="person"></ion-icon> Daftar Akun</a></li>
           </ul>
         </ul>
       </div>
@@ -217,6 +217,33 @@
         </footer>
       </div>
     </div>
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="registerModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header" style="background-color:#ffbc3e; padding-vertical:0;">
+            <h6 class="modal-title" id="registerModalLabel">Register Calon Pegawai</h6>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form action="">
+              <div class="form-group">
+                <input type="email" class="form-control" id="registerInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+              </div>
+              <div class="form-group">
+                <input type="password" class="form-control" id="registerInputPassword1" placeholder="Password">
+              </div>
+              <button type="submit" class="btn btn-success">Sign In</button>
+            </form>
+        </div>
+      </div>
+    </div>
+    </div>
+
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
@@ -240,6 +267,8 @@
         </div>
       </div>
     </div>
+    </div>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>  
--- application/views/V_luluseleksi.php
+++ application/views/V_luluseleksi.php
@@ -10,34 +10,38 @@
     <link rel="icon" href="<?= base_url("assets/img/g.png")?>" type="image/gif" sizes="20x20" >
 </head>
 <body>
-    <nav class="navbar navbar-expand-sm fixed-top">
+    <nav class="navbar navbar-expand-sm fixed-top" style="font-size : 14px;">
         <div class="collapse navbar-collapse "  id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-link"><a href="<?= site_url("C_dpegawai")?>" id="clr"><ion-icon name="home"></ion-icon> Halaman Rekrutmen </a></li>
-                <li class="nav-link"><a  href="<?= site_url("C_editprofil")?>" id="clr"><ion-icon name="information-circle"></ion-icon> Pengumuman Lolos Seleksi</a></li>
+            <ul class="navbar-nav mr-auto" style="margin-left : 115px;">
+                <li class="nav-link"><a href="<?= site_url("C_dpegawai")?>" id="clr" style="font-size : 14px;"><ion-icon name="home"></ion-icon> Halaman Rekrutmen </a></li>
+                <li class="nav-link"><a  href="<?= site_url("C_luluseleksi")?>" id="clr"><ion-icon name="information-circle"></ion-icon> Pengumuman Lolos Seleksi</a></li>
             </ul>
         </div>
         <div class="collapse navbar-collapse navbar-inline dropdown justify-content-end">
             <ul class="nav navbar-nav navbar-right">
             <li class="dropdown">
-            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" id="clr"><ion-icon name="logo-windows"></ion-icon>  Login Calon Pegawai</a>
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" id="clr" style="margin-right : 110px;"><ion-icon name="logo-windows"></ion-icon>  Login Calon Pegawai</a>
             <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Admin PKL</a></li>
-                <li><a class="dropdown-item" href="#">Admin Pegawai</a></li>
+                <li><a class="dropdown-item" href="<?= site_url("C_editprofil")?>">Edit Akun</a></li>
+                <li><a class="dropdown-item" href="#">Sign Out</a></li>
             </ul>
             </li>
             </ul>
         </div>
     </nav>
-    <div class="container" style="background-color: #fff">
-        <img class="img-fluid rounded-lg mt-3 mb-2" src="<?= base_url("assets/img/rek.jpg")?>">               
-        <ul class="breadcrumb">
-          <li class="breadcrumb-item"><a href="#">Dashboard </a></li>
-          <li class="breadcrumb-item"><a href="<?= site_url('C_dpegawai')?>"></a>Halaman Rekrutmen</li>
-          <li class="breadcrumb-item active">Peserta Lolos Seleksi</li>
-        </ul>   
-        <h3>Daftar Peserta Lolos Seleksi</h3>
-            <div class="mini-layout">
+    <div class="container">
+        <img class="img-fluid rounded-lg mt-3 mb-2" src="<?= base_url("assets/img/rek.jpg")?>">
+        <div class="row pb-2">
+            <div class="col-md-12">
+                <div class="wrap bg-white rounded-lg p-4">
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Dashboard </a></li>
+                        <li class="breadcrumb-item"><a href="#">Halaman Rekrutmen</a></li>
+                        <li class="breadcrumb-item active">Peserta Lulus Seleksi</li>
+                    </ul>
+                <div>
+    		<h3>Daftar Peserta Lolos Seleksi</h3>
+            <div class="mini-layout" style="border : 1px solid; padding : 9px; border-color : #DDD;">
                 <div class="table-responsive align:center">
                     <table class="table table-bordered">
                         <tbody>
@@ -51,8 +55,10 @@
                     </table>
                 </div>
             </div>
-        <footer class="pt-3" style="border-top : 1px solid rgb(175,0,0);">
-        <p>© Global Intermedia</p>
+        </div>
+    </div>
+    <footer class="pt-3" style="border-top : 1px solid rgb(175,0,0);">
+          <p>© Global Intermedia</p>
         </footer>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
